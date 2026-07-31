3fd6cec951d27e58539a3a537c1185cdee103ce4
c-m-hall <165726572+c-m-hall@users.noreply.github.com>
2026-07-31 14:06:06 -0700

    Fix output filename formatting for processed FITS files
    Updated output filename formatting to replace '/' with '.' for processed FITS files.

--- module_resample/resample.py
+++ module_resample/resample.py
@@ -212,6 +212,7 @@ def resample_main(z,cubepath, pixscale):
 
     # write the HDUList to a new FITS file
     output_filename = cubepath.split('.fits')[0] + '_processed_cube.fits'
+    output_filename = output_filename.replace('/', '.')
     hdul.writeto(output_filename, overwrite=True)
 
 
@@ -335,6 +336,7 @@ def resample_mask_main(z, maskpath, pixscale, dv=25.0, target_kpc=2.0,
 
     hdul_out = fits.HDUList([primary_hdu, vgrid_hdu])
     output_filename = maskpath.split('.fits')[0] + '_processed_mask.fits'
+    output_filename = output_filename.replace('/', '.')
     hdul_out.writeto(output_filename, overwrite=True)
 
     return output_filename
