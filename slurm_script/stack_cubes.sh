27fea10f0fe6f35063e54c73cd793d6a8cfdccbb
c-m-hall <165726572+c-m-hall@users.noreply.github.com>
2026-08-09 10:10:43 -0700

    Add files via upload

---
+++ slurm_script/stack_cubes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=stack_cubes
+#SBATCH --partition=obs
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
+#SBATCH --time=02:00:00
+#SBATCH --output=slurm_script/process_cubes_%A.out
+#SBATCH --error=slurm_script/process_cubes_%A.err
+
+module load conda
+conda activate /carnegie/scidata/groups/muse_stacking/conda_envs/cassi
