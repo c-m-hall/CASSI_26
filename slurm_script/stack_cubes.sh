d36a18172a94558fd3340455474738d5f09fe0e3
c-m-hall <165726572+c-m-hall@users.noreply.github.com>
2026-08-09 12:50:01 -0700

    Update stack_cubes.sh

--- slurm_script/stack_cubes.sh
+++ slurm_script/stack_cubes.sh
@@ -5,8 +5,8 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10G
 #SBATCH --time=02:00:00
-#SBATCH --output=slurm_script/stack_cubes_%A.out
-#SBATCH --error=slurm_script/stack_cubes_%A.err
+#SBATCH --output=/carnegie/scidata/groups/muse_stacking/CASSI_26/slurm_script/stack_cubes_%A.out
+#SBATCH --error=/carnegie/scidata/groups/muse_stacking/CASSI_26/slurm_script/stack_cubes_%A.err
 
 module load conda
 conda activate /carnegie/scidata/groups/muse_stacking/conda_envs/cassi
