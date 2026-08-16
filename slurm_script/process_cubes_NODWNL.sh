5b82308f1a229015e85768390ad375bcb7e9a886
c-m-hall <165726572+c-m-hall@users.noreply.github.com>
2026-08-16 13:17:09 -0700

    Rename SLURM job for no-download processing

--- slurm_script/process_cubes_NODWNL.sh
+++ slurm_script/process_cubes_NODWNL.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=cassi_process
-#SBATCH --output=slurm_script/cassi_process_%j.out
-#SBATCH --error=slurm_script/cassi_process_%j.err
+#SBATCH --job-name=nodownload_process
+#SBATCH --output=slurm_script/nodownload_process_%j.out
+#SBATCH --error=slurm_script/nodownload_process_%j.err
 #SBATCH --time=24:00:00
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=4
+
+
 
 module load conda
 conda activate /carnegie/scidata/groups/muse_stacking/conda_envs/cassi
